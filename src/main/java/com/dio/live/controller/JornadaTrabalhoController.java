6dabf5d7bdd2737e9de8ae592ef2625b22c3aa16
Caio Lima <caio.lima1991@icloud.com>
2021-09-02 23:07:04 -0300

    Primeiro Commit

---
+++ src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
@@ -0,0 +1,48 @@
+package com.dio.live.controller;
+
+import com.dio.live.model.JornadaTrabalho;
+import com.dio.live.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/jornada")
+public class JornadaTrabalhoController {
+
+    @Autowired
+    JornadaService jornadaService;
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.saveJornada(jornadaTrabalho);
+    }
+
+    @GetMapping
+    public List<JornadaTrabalho> getJornadaList(){
+        return jornadaService.findAll();
+    }
+
+    @GetMapping("/{idJornada}")
+    public ResponseEntity<JornadaTrabalho> getJornadaByID(@PathVariable("idJornada") long idJornada){
+        return ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new NoSuchElementException("Jornada não localizada.")));
+    }
+
+    @PutMapping
+    public JornadaTrabalho updateJornada(@RequestBody JornadaTrabalho jornadaTrabalho) {
+        return jornadaService.updateJornada(jornadaTrabalho);
+    }
+
+    @DeleteMapping("/{idJornada}")
+    public ResponseEntity deleteByID(@PathVariable("idJornada") Long idJornada) {
+        try {
+            jornadaService.deleteJornada(idJornada);
+        } catch(Exception ex) {
+            return (ResponseEntity) ResponseEntity.notFound();
+        }
+        return (ResponseEntity) ResponseEntity.ok();
+    }
+}
